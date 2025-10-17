e423bd7aab17f38f37f1bf45766b5989f9fd31fe
Luffy-lulu <146710770+LuluH19@users.noreply.github.com>
2025-10-17 16:11:04 +0200

    1er test RED

--- template_TS/index.test.ts
+++ template_TS/index.test.ts
@@ -1,11 +1,15 @@
 
 import { describe, test, expect } from "@jest/globals"
 
+class Panier {
+    getNombreProduits(): number {
+        return 0;
+    }
+}
 
 describe('Panier E-commerce - TDD', () => {
     test('devrait créer un panier vide', () => {
         const panier = new Panier();
         expect(panier.getNombreProduits()).toBe(0);
     });
-    
 });
